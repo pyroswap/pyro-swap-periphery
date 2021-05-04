57e7fdcb37fa887dbd9724049beb99533a4c8f75
panther-swap <admin@pantherswap.com>
2021-05-04 11:47:55 +0800

    Update pancake to panther

--- contracts/interfaces/IPancakeRouter02.sol
+++ contracts/interfaces/IPancakeRouter02.sol
@@ -1,8 +1,8 @@
 pragma solidity >=0.6.2;
 
-import './IPancakeRouter01.sol';
+import './IPantherRouter01.sol';
 
-interface IPancakeRouter02 is IPancakeRouter01 {
+interface IPantherRouter02 is IPantherRouter01 {
     function removeLiquidityETHSupportingFeeOnTransferTokens(
         address token,
         uint liquidity,
